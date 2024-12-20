6bcc4bf59643f3ec7ce17d0a534b64c93b6c20b7
Dan Gowans <dangowans@users.noreply.github.com>
2024-12-20 13:27:06 -0500

    update eslint-config-cityssm

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,20 @@
-import configCityssm from 'eslint-config-cityssm';
+import configCityssm, { cspellWords } from 'eslint-config-cityssm';
 import tseslint from 'typescript-eslint';
 export const config = tseslint.config(...configCityssm, {
     rules: {
-        '@typescript-eslint/no-unsafe-argument': 'warn',
+        '@cspell/spellchecker': [
+            'warn',
+            {
+                cspell: {
+                    words: [
+                        ...cspellWords,
+                        'actv',
+                        'wmitm'
+                    ]
+                }
+            }
+        ],
         '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-call': 'warn',
-        '@typescript-eslint/no-unsafe-member-access': 'warn',
-        '@typescript-eslint/no-unsafe-return': 'warn',
         'unicorn/no-null': 'warn'
     }
 });
